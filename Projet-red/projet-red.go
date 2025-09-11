e537efc42aa5da5634739d5ca1e7d989e5f4133c
Broqué Lilian <lilian040605@gmail.com>
2025-09-11 14:39:33 +0200

    first commit task 1

---
+++ Projet-red/projet-red.go
@@ -0,0 +1,18 @@
+package projetrednomprojet
+
+type character struct {
+	name       string
+	domaine    string
+	niv        int
+	pv_max     int
+	pv_act     int
+	inventaire []string
+}
+
+func initCharacter() {
+	var perso1 character
+}
+
+func main() {
+	initCharacter.perso1.name
+}
